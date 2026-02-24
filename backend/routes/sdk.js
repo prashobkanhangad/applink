2022d2ce432ffc3526afb8201735c99f59abd028
gokul9642 <gokul@erlyjobs.in>
2026-02-24 17:12:45 +0530

    imported deeplink click controller

--- backend/routes/sdk.js
+++ backend/routes/sdk.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { getAllPlans } from "../controllers/plans/plans.controller.js";
-import { handleTrackInstall } from "../controllers/sdk/sdk.controller.js";
+import { handleTrackInstall, deeplinkClick } from "../controllers/sdk/sdk.controller.js";
 const sdkRoute = Router();
 
 // route to get all plans
     sdkRoute.post("/install",handleTrackInstall);
+
+    sdkRoute.post("/deeplink",deeplinkClick);
 
 
 export default sdkRoute;
