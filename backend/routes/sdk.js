71deb30965ddd4b4cd4c9c20abb3a17d6fd91cc1
gokul9642 <gokul@erlyjobs.in>
2026-03-01 19:00:25 +0530

    added middleware for apikey val

--- backend/routes/sdk.js
+++ backend/routes/sdk.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { getAllPlans } from "../controllers/plans/plans.controller.js";
 import { handleTrackInstall, deeplinkClick } from "../controllers/sdk/sdk.controller.js";
+import { apiKeyValidation } from "../middleware/apikey.js";
 const sdkRoute = Router();
 
 // route to get all plans
+
+
+    sdkRoute.use(apiKeyValidation);
+
     sdkRoute.post("/install",handleTrackInstall);
 
     sdkRoute.post("/deeplink",deeplinkClick);
